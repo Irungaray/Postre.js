8bcf33af7e9b852a385dec263f51fdf88739b7c7
Irungaray <lionelirungaray@gmail.com>
2022-04-06 17:27:07 -0300

    Setup TypeORM, first entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'user',
+      password: 'postgres',
+      database: 'my_db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+    }),
+    TasksModule,
+  ],
   controllers: [],
   providers: [],
 })
