600ce85f974309dbf501c5adf290ba242b0307c5
Irungaray <lionelirungaray@gmail.com>
2022-04-07 10:14:26 -0300

    Fix dependency break

--- src/tasks/services/tasks.service.ts
+++ src/tasks/services/tasks.service.ts
@@ -1,4 +1,41 @@
+// Ext modules
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
+// Int modules
+import { Task } from '../entities/task.entity';
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectRepository(Task) private tasksRepo: Repository<Task>) {}
+
+  findAll() {
+    return this.tasksRepo.find();
+  }
+
+  findOne(id: number) {
+    return this.tasksRepo.findOneBy({ id });
+  }
+
+  create(body: any) {
+    const newTask = new Task();
+
+    newTask.name = body.name;
+
+    return this.tasksRepo.save(newTask);
+  }
+
+  async update(id: number, body: any) {
+    const task = await this.tasksRepo.findOneBy({ id });
+
+    this.tasksRepo.merge(task, body);
+
+    return this.tasksRepo.save(task);
+  }
+
+  async delete(id: number) {
+    await this.tasksRepo.delete(id);
+
+    return true;
+  }
+}
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,8 +1,14 @@
+// Ext modules
 import { Module } from '@nestjs/common';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+// Int modules
+import { Task } from './entities/task.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Task])],
   providers: [TasksService],
   controllers: [TasksController],
 })
